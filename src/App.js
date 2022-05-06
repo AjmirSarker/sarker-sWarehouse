a1bdcc1e960278e7aa0a50faee21d7c54e743856
AjmirSarker <89391451+AjmirSarker@users.noreply.github.com>
2022-05-06 17:22:54 +0600

    updatingsrc

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import NotFound from './components/NotFound/NotFound';
+import Header from './components/Header/Header';
+import LogIn from './components/LogIn/LogIn';
+import SignUp from './components/SignUp/SignUp';
+import Inventory from './components/Inventory/Inventory';
+import Blogs from './components/Blogs/Blogs';
+import AllProducts from './components/AllProducts/AllProducts';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/login' element={<LogIn></LogIn>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
+        <Route path='/inventory' element={<Inventory></Inventory>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/allProducts' element={<AllProducts></AllProducts>}></Route>
+        
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
      
